496e632d02429d42671f62442c96bfa126e64d78
qiu <qiujiaxp@163.com>
2024-10-29 14:17:13 +0800

    feat: modify the interaction for setting the mode

--- src/components/user-info/index.tsx
+++ src/components/user-info/index.tsx
@@ -2,37 +2,27 @@ import React, { useState } from 'react';
 import { Dropdown, Space, Menu, Avatar } from 'antd';
 import { signOut, useSession } from 'next-auth/react';
 import { DownOutlined } from '@ant-design/icons';
-import SettingsModal from './settings';
+import Theme from '@/components/theme';
+import { useTranslation } from '@/utils/i18n';
 
 const UserInfo = () => {
   const { data: session } = useSession();
+  const { t } = useTranslation();
   const username = session?.username || 'Qiu-Jia';
-  const [visible, setVisible] = useState<boolean>(false);
 
   const handleLogout = () => {
     signOut({ callbackUrl: '/' });
   };
 
-  const handleSettings = () => {
-    setVisible(true);
-  };
-
   const items: Array<{ label: JSX.Element; key: string } | { type: 'divider' }> = [
     {
-      label: <a onClick={handleSettings}>Settings</a>,
-      key: '0',
-    },
-    {
-      type: 'divider',
-    },
-    {
-      label: <a onClick={handleLogout}>Logout</a>,
-      key: '1',
-    },
+      label: <a onClick={handleLogout}>{t('common.logout')}</a>,
+      key: '',
+    }
   ];
 
   return (
-    <div>
+    <div className='flex'>
       {username && (
         <Dropdown overlay={<Menu items={items} />} trigger={['click']}>
           <a className='cursor-pointer' onClick={(e) => e.preventDefault()}>
@@ -51,9 +41,7 @@ const UserInfo = () => {
           </a>
         </Dropdown>
       )}
-      <SettingsModal 
-        visible={visible} 
-        onClose={() => setVisible(false)} />
+      <Theme />
     </div>
   );
 };
